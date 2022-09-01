8134f6a87cf6e5424731bc2c45213a476a10cd09
Veidz <joaovictorveidz@gmail.com>
2022-09-01 11:40:32 -0300

    test: ensure SignUpService returns status 400 with correct message if invalid name is provided

--- src/services/signup.spec.ts
+++ src/services/signup.spec.ts
@@ -1,13 +1,33 @@
 import { BadRequest } from '../errors/bad-request'
+import { Validators } from '../protocols/validators'
 import { SignUpService } from './signup'
 
 interface SutTypes {
   sut: SignUpService
+  validators: Validators
+}
+
+const makeValidators = (): Validators => {
+  class SignUpValidationsStub implements Validators {
+    isNameValid (name: string): boolean {
+      return true
+    }
+
+    isEmailValid (email: string): boolean {
+      return true
+    }
+
+    isPasswordValid (password: string): boolean {
+      return true
+    }
+  }
+  return new SignUpValidationsStub()
 }
 
 const makeSut = (): SutTypes => {
+  const validators = makeValidators()
   const sut = new SignUpService()
-  return { sut }
+  return { sut, validators }
 }
 
 describe('SignUpService', () => {
@@ -68,6 +88,22 @@ describe('SignUpService', () => {
       await sut.create(httpRequest)
     } catch (error) {
       expect(error).toEqual(new BadRequest('passwordConfirmation is required'))
+    }
+  })
+
+  test('Should return status 400 with correct message if invalid name is provided', async () => {
+    try {
+      const { sut, validators } = makeSut()
+      jest.spyOn(validators, 'isNameValid').mockReturnValueOnce(false)
+      const httpRequest = {
+        name: 'invalid_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+      await sut.create(httpRequest)
+    } catch (error) {
+      expect(error).toEqual(new BadRequest('Invalid name'))
     }
   })
 })
