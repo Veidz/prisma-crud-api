b17ac0eb89966c4d7666eb30de72031631794bb1
Veidz <joaovictorveidz@gmail.com>
2022-08-31 21:43:10 -0300

    feat: ensure SignUpService returns status 400 with correct message if no email is provided

--- src/services/signup.ts
+++ src/services/signup.ts
@@ -1,5 +1,6 @@
 export class SignUpService {
   public create (userPayload: any): any {
     if (!userPayload.name) return { statusCode: 400, message: 'Name is required' }
+    if (!userPayload.email) return { statusCode: 400, message: 'Email is required' }
   }
 }
