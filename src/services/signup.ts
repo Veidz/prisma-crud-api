ebb067b72783ff0526ae5e2614cbe7713927f53b
Veidz <joaovictorveidz@gmail.com>
2022-08-31 21:44:32 -0300

    feat: ensure SignUpService returns status 400 with correct message if no password is provided

--- src/services/signup.ts
+++ src/services/signup.ts
@@ -2,5 +2,6 @@ export class SignUpService {
   public create (userPayload: any): any {
     if (!userPayload.name) return { statusCode: 400, message: 'Name is required' }
     if (!userPayload.email) return { statusCode: 400, message: 'Email is required' }
+    if (!userPayload.password) return { statusCode: 400, message: 'Password is required' }
   }
 }
