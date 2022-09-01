0ff92960bfd74476d83110b4158414ebc98dbec0
Veidz <joaovictorveidz@gmail.com>
2022-09-01 17:39:02 -0300

    fix: add rule in tsconfig to prisma client

--- tests/signup.test.ts
+++ tests/signup.test.ts
@@ -3,6 +3,9 @@ import { app } from '../src/app'
 
 import { UserPayload } from '../src/protocols/user-payload'
 
+import { prismaMock } from '../src/database/singleton'
+import { Users } from '@prisma/client'
+
 const userPayload: UserPayload = {
   name: 'any_name',
   email: 'any_email@email.com',
@@ -10,8 +13,18 @@ const userPayload: UserPayload = {
   passwordConfirmation: 'any_password'
 }
 
+const userPayloadDB: Users = {
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@email.com',
+  password: 'any_password',
+  created_at: new Date(),
+  updated_at: new Date()
+}
+
 describe('POST /signup', () => {
   test('Should return status 201 with correct message if user is successfully created', async () => {
+    prismaMock.users.create.mockResolvedValue(userPayloadDB)
     await request(app)
       .post('/signup')
       .send(userPayload)
