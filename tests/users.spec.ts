2ccd27735d08e4343e30051dd593efcbb44e179e
Veidz <joaovictorveidz@gmail.com>
2022-09-01 20:20:16 -0300

    test: ensure UsersService returns correct users from the database

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -1,6 +1,26 @@
-import { UsersService } from '../src/services/users'
+import { Users } from '@prisma/client'
 import { prismaMock } from '../src/database/singleton'
+import { UsersService } from '../src/services/users'
+import { UsersWithoutPassword } from '../src/protocols'
 import { NotFound } from '../src/errors'
+
+const usersDB: Users[] = [
+  {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@email.com',
+    password: 'any_password',
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+]
+
+const usersWithoutPassword: UsersWithoutPassword[] = [
+  {
+    name: 'any_name',
+    email: 'any_email@email.com'
+  }
+]
 
 describe('UsersService', () => {
   test('Should return correct error if no users are in the database', async () => {
@@ -11,5 +31,12 @@ describe('UsersService', () => {
     } catch (error) {
       expect(error).toEqual(new NotFound('No users in the database'))
     }
+  })
+
+  test('Should return correct users from the database', async () => {
+    const sut = new UsersService()
+    jest.spyOn(prismaMock.users, 'findMany').mockResolvedValue(usersDB)
+    const users = await sut.findMany()
+    expect(users).toEqual(usersWithoutPassword)
   })
 })
